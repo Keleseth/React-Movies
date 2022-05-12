04960a204a0babcb666815008b3d59e049626866
Olecsiy Gavrilyuk <infernoregent@gmail.com>
2022-05-12 19:17:53 +0300

    complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './layout/Footer';
+import Header from "./layout/Header";
+import Main from './layout/Main'
+
+const App = () => {
+   return (
+       <React.Fragment>
+       <Header />
+       <Main />
+       <Footer />
+       </React.Fragment>
+   )
+}
+
+export default App;
